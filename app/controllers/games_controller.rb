26f01f205384b2f932b2789ceeca86200fab77bb
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-06-17 13:39:16 +0000

    Add games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,32 @@
 class GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to games_path
+  end
+
+private
+
+  def game_params
+    params.repuire(:game).permit(:name)
+  end
+
 end
