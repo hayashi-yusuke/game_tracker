43f01b897775dad896aafe49c6bc5b89dce28187
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-06-17 14:32:14 +0000

    Add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+  def create
+    @session = Session.new(game_id: params[:game_id], started_at: Time.current)
+    @session.save
+  end
+
+  def update
+    @session = Session.find(params[:id])
+    @session.update(ended_at: Time.current)
+  end
+  
 end
